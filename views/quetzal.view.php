eff78da26bb24db6bd740fc1ae419ffdfdb44299
Xibalsba <jl573160@gmail.com>
2019-11-06 22:35:29 -0600

    LLamar archivos de boostrap en la vista

--- views/quetzal.view.php
+++ views/quetzal.view.php
@@ -2,9 +2,12 @@
 <html lang="es">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Quetzal</title>
+    <link rel="stylesheet" href="public/plugins/bootstrap/css/bootstrap.min.css">
   </head>
   <body>
     <h1>Hola desde Quetzal</h1>
+
+    <script src="public/plugins/bootstrap/js/bootstrap.min.js"></script>
   </body>
 </html>
