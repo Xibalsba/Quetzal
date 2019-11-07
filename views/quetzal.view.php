48ab08eba325d5a5b368c743529a60afca6c8857
Xibalsba <jl573160@gmail.com>
2019-11-06 22:50:53 -0600

    Integración de jquery, vue y axios en la plantilla

--- views/quetzal.view.php
+++ views/quetzal.view.php
@@ -8,6 +8,9 @@
   <body>
     <h1>Hola desde Quetzal</h1>
 
+    <script src="public/pluging/jquery/jquery-3.4.1.min.js" charset="utf-8"></script>
     <script src="public/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="public/plugins/vue/vue.min.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   </body>
 </html>
