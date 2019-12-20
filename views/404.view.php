5e375f5e8fa41bb99314a47fa03e778b30968dc2
Xibalba <jl573160@gmail.com>
2019-12-20 15:38:40 -0600

    [update] Error 404

--- views/404.view.php
+++ views/404.view.php
@@ -1,3 +1,24 @@
 <?php include "system/header.php"; ?>
-  <h1>¡Vaya! parece que la url que solicitas no existe :(</h1>
+  <style>
+  .container{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    height: 30%;
+    width: 50%;
+    margin: -15% 0 0 -25%;
+  }
+  h1{
+    font-size: 10rem;
+    letter-spacing: 1rem;
+  }
+  p{
+    color:#6a6a6a;
+    letter-spacing: 0.1rem;
+  }
+</style>
+  <div class="container text-center">
+    <h1>404</h1>
+    <p>¡Oh vaya! parece que has entrado a un portal desconocido...</p>
+  </div>	
 <?php include "system/footer.php"; ?>
