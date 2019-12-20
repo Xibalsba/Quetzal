03f2b81923aa8981bf0342386381b5c0fada0e8a
Xibalba <jl573160@gmail.com>
2019-12-20 15:35:37 -0600

    [update] Mensaje de bienvenida

--- views/quetzal.view.php
+++ views/quetzal.view.php
@@ -1,3 +1,25 @@
 <?php include "system/header.php"; ?>
-  <h1>Hola desde Quetzal</h1>
+<style>
+  .container{
+    position: absolute;
+    top: 70%;
+    left: 50%;
+    height: 30%;
+    width: 50%;
+    margin: -15% 0 0 -25%;
+  }
+  h1{
+    font-size: 3.5rem;
+    letter-spacing: 1rem;
+  }
+  p{
+    color:#6a6a6a;
+    letter-spacing: 0.1rem;
+  }
+</style>
+  <div class="container text-center">
+    <h1>QUETZAL</h1>
+    <p>Es una plantilla de trabajo para Vuejs y PHP.</p>
+    <a href="https://github.com/Xibalsba/Quetzal" class="btn btn-dark" target="_blank">Github</a>
+  </div>
 <?php include "system/footer.php"; ?>
