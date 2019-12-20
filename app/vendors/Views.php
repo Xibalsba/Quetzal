ecdd5ea7ba38d281fc56ac7033f1ffe7734a78cb
Xibalba <jl573160@gmail.com>
2019-12-20 15:13:37 -0600

    [add] Manejo de url que no existen 404

--- app/vendors/Views.php
+++ app/vendors/Views.php
@@ -5,6 +5,8 @@ class View{
   public function render($link){
     if($link == "quetzal"){
       $view = "views/".$link.".view.php";
+    }else{
+      $view = "views/404.view.php";
     }
     return $view;
   }
---
+++ views/404.view.php
@@ -0,0 +1,3 @@
+<?php include "system/header.php"; ?>
+  <h1>¡Vaya! parece que la url que solicitas no existe :(</h1>
+<?php include "system/footer.php"; ?>
