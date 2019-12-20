b53aa94ef64e4ff4ed6cd1c3afbc557b01ef91a4
Xibalba <jl573160@gmail.com>
2019-12-20 14:49:58 -0600

    [add] Meta Viewport

--- views/system/header.php
+++ views/system/header.php
@@ -2,7 +2,8 @@
 <html lang="es">
   <head>
     <meta charset="utf-8">
-    <title>Quetzal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="public/plugins/bootstrap/css/bootstrap.min.css">
+    <title>Quetzal</title>
   </head>
   <body>
