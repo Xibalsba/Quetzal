9edf723736d3b13a8743918abfab4256bd6b1317
Xibalba <jl573160@gmail.com>
2019-12-20 14:47:26 -0600

    [update] Separar el header y el footer de la página para no repetir la estructura HTML

--- views/quetzal.view.php
+++ views/quetzal.view.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-  <head>
-    <meta charset="utf-8">
-    <title>Quetzal</title>
-    <link rel="stylesheet" href="public/plugins/bootstrap/css/bootstrap.min.css">
-  </head>
-  <body>
-    <h1>Hola desde Quetzal</h1>
-
-    <script src="public/plugins/jquery/jquery-3.4.1.min.js" charset="utf-8"></script>
-    <script src="public/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script src="public/plugins/vue/vue.min.js"></script>
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-  </body>
-</html>
+<?php include "system/header.php"; ?>
+  <h1>Hola desde Quetzal</h1>
+<?php include "system/footer.php"; ?>
---
+++ views/system/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>Quetzal</title>
+    <link rel="stylesheet" href="public/plugins/bootstrap/css/bootstrap.min.css">
+  </head>
+  <body>
